2816a4d503eeb306474faedb6cb2d8fc2769d2c9
Mohsen tahmasebi <mohsen5758@gmail.com>
2021-02-18 23:32:41 +0330

    second job

--- panel/models.py
+++ panel/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class proplem (models.Model):
+    name=models.CharField(max_length=255)
+    date=models.DateTimeField()
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
