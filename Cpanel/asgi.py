6e2941a8f9ec8ef834c5cad96c8bde09e6c294e0
Mohsen tahmasebi <mohsen5758@gmail.com>
2021-02-18 22:25:31 +0330

    first job

---
+++ Cpanel/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Cpanel project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cpanel.settings')
+
+application = get_asgi_application()
